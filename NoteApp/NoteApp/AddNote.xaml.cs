08ce391b6e3648487724c9bcf0aa1f50fe7f172e
MercieKimaku <merciekimaku@gmail.com>
2016-11-03 06:33:53 -0700

    Add files via upload

---
+++ NoteApp/NoteApp/AddNote.xaml.cs
@@ -0,0 +1,45 @@
+﻿using NoteApp.DataModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace NoteApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddNote : Page
+    {
+       // private bool isViewing = false;
+       // private Note myNote;
+        public AddNote()
+        {
+            this.InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ NoteApp/NoteApp/DataModel/DataSource.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace NoteApp.DataModel
+{
+    // class that holds the properties of a note
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Notes { get; set; }
+        public DateTime DateCreated { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+    public class DataSource
+    {
+        private ObservableCollection<Note> _myNotes;
+        const string filename = "mapnotes.json";
+        public DataSource()
+        {
+            _myNotes = new ObservableCollection<Note>();
+        }
+
+        public async Task<ObservableCollection<Note>> GetMapNotes()
+        {
+            await ensureDataLoaded();
+            return _myNotes;
+        }
+
+        private async Task ensureDataLoaded()
+        {
+            if (_myNotes.Count == 0)
+            {
+                await getMapNoteDataAsync();
+            }
+        }
+
+        private async Task getMapNoteDataAsync()
+        {
+            if (_myNotes.Count != 0)
+                return;
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<Note>));
+            try
+            {
+                using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForReadAsync(filename))
+                {
+                    _myNotes = (ObservableCollection<Note>)jsonSerializer.ReadObject(stream);
+
+                }
+            }
+            catch
+            {
+                _myNotes = new ObservableCollection<Note>();
+            }
+
+        }
+
+        public async void AddNote(Note myNote)
+        {
+            _myNotes.Add(myNote);
+            await saveNoteDataAsync();
+        }
+
+        private async Task saveNoteDataAsync()
+        {
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<Note>));
+            using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync(filename,
+                CreationCollisionOption.ReplaceExisting))
+            {
+                jsonSerializer.WriteObject(stream, _myNotes);
+            }
+
+        }
+
+        public async void DeleteMapNote(Note myNote)
+        {
+            _myNotes.Remove(myNote);
+            await saveNoteDataAsync();
+        }
+
+    }
+}
